5f4274e4a07fb37c5c0cf194e650c9e3e98e224c
RomanFirstt <roman.asalkhanov@gmail.com>
2023-08-22 20:47:32 +0200

    update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,7 @@
 import ProjectItem from '../components/ProjectItem'
+import portfolioImg from '../public/assets/projects/portfolio.png'
+import shopImg from '../public/assets/projects/shop.png'
+import stoImg from '../public/assets/projects/sto-103.png'
 const Projects = () => {
 	return (
 		<div id='projects' className='w-full'>
@@ -8,7 +11,24 @@ const Projects = () => {
 				</p>
 				<h2 className='py-4'>What I've Built</h2>
 				<div className='grid md:grid-cols-2 gap-8'>
-					<ProjectItem />
+					<ProjectItem
+						title='Garage 103'
+						backgroundImg={stoImg}
+						projectUrl='https://sto-103-tau.vercel.app/'
+						technologies='React/TypeScript'
+					/>
+					<ProjectItem
+						title='Portfolio UX/UI'
+						backgroundImg={portfolioImg}
+						projectUrl='https://portfolio-ux-ui.vercel.app/'
+						technologies='HTML/CSS/JS'
+					/>
+					<ProjectItem
+						title='Home Familia'
+						backgroundImg={shopImg}
+						projectUrl='https://sto-103-tau.vercel.app/'
+						technologies='React/TypeScript'
+					/>
 				</div>
 			</div>
 		</div>
